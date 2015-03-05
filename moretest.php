0ce0962f5676bf217d7148cd76e6d3c6118580e9
Calypso <tbrown81@gmail.com>
2015-03-04 19:53:18 -0600

    rollsOnly.php totals pins without strike & spare counts

--- moretest.php
+++ moretest.php
@@ -14,19 +14,15 @@ var p = [];
 var counter=1;
 var inSide=1;
 function anotherStrike(){
-	var arr = [
-	document.getElementById("roll2"),
-	document.getElementById("roll3")	
-	]
 	if(pinCnt>9){
 		console.log("Strike");
+		p.push();
 		if(inSide){
 			counter=rollnum;
 		}else{
 			inside=0;
 		}
 	}	
-	p.push(pinCnt);
 	console.log(p.toString());
 	rollnum++;
 }
@@ -38,8 +34,6 @@ function anotherStrike(){
 
 
 <input type="text" id="roll1"></input>
-<input type="text" id="roll2"></input>
-<input type="text" id="roll3"></input>
 <input type="button" onclick="anotherStrike();" value="Strike"></input>
 
 
