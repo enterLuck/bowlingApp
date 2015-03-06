2cde6c90946f9b4ee0ecfbb9832b82f03695111b
Calypso <tbrown81@gmail.com>
2015-03-05 19:51:44 -0600

    on to something with calculations

--- recalculate.js
+++ recalculate.js
@@ -1,4 +1,4 @@
-function recalculateFrame1(pinCount){
+function calcSpare(pinCount){
 	//placed on frame 2 roll 1
 	if(fr.frame1spare ==="/"){
 		fr.frame1storeTotal = parseInt(fr.frame1total()) + parseInt(pinCount);
@@ -6,3 +6,21 @@ function recalculateFrame1(pinCount){
 		console.log("No recalculation necessary!");
 	}
 }
+function calcStrike(rolls){
+	if(fr.frame1strike === "X"){
+		if(fr.frame2strike === "X"){
+
+		}else if(fr.frame2spare === "/"){
+
+		}
+		else{
+		fr.frame1storeTotal = parseInt(fr.frame1total()) + parseInt(rolls);
+		}
+	}
+}
+function calcStrikeSpare(){
+
+}
+function calcStrikeStrike(){}
+
+
